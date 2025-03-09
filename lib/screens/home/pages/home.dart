73fb0f46d5646fde7225418b19e8e040fbcf6f47
najifa6053 <esha15-6053@diu.edu.bd>
2025-03-09 11:27:01 +0600

    appbar

---
+++ lib/screens/home/pages/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../../../common/widgets/app_bar.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: FAppBar(),
+    );
+  }
+}
